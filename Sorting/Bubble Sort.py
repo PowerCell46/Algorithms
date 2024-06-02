727d12707ac312ee9219d0b8c30a405f952189c7
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2024-06-02 10:09:46 +0300

    Update Bubble Sort.py

--- Sorting/Bubble Sort.py
+++ Sorting/Bubble Sort.py
@@ -1,27 +1,40 @@
-# Bubble Sort
+# Insertion Sort
 
-# Starting from the first element, we compare every two values and move the bigger one to the right.
-# When we reach the end, we know the last element is the biggest one, so on the next iteration, we start again from the
-# first, but this time we do one less comparing in the end.
+# Starting from the second element, we compare the current value with the previous elements
+# This way the sublist on the left side is always sorted.
+# Using a nested loop, we start from the last element of the sublist and compare it with the current value
+# If the inspected value is greater than the current value, we move it to the right
+# else we put the current value in the next inspected value's index
+# If we reach the first element of the sublist, we put the current value in the first index
 
-def bubble_sort(lst):
-    list_length = len(lst)
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        current_value = lst[i]
+        lst[i] = None
+        
+        # print(f'Ordered list on the left side: {lst[:i]}.'
+        #       f'\nCurrent value: {current_value}.'
+        #       f'\nCurrent list: {lst}.'
+        #       f'\n---')
 
-    while list_length > 0:
-
-        for index in range(1, list_length):
-            previous_value = lst[index - 1]
-            current_value = lst[index]
-            if previous_value > current_value:
-                lst[index - 1], lst[index] = lst[index], lst[index - 1]
+        for index in range(i - 1, -1, -1):
+            inspected_value = lst[index]
+            if inspected_value > current_value:
+                lst[index + 1], lst[index] = lst[index], None
+                # If the inspected element is greater, we shift it to the right
+                # With setting the current index to None we remove it's redundant value
+            else:
+                lst[index + 1] = current_value
+                # If the inspected value is less, then we put the current value in the next inspected value's index
+                break
         else:
-            list_length -= 1
+            lst[lst.index(None)] = current_value
 
     return lst
 
 
-lst = [5, 3, 8, 6, 7, 2]
+lst = [5, 3, 1, 2, 4]
 
-sorted_lst = bubble_sort(lst)
+sorted_list = insertion_sort(lst)
 
-print(sorted_lst)
+print(sorted_list)
